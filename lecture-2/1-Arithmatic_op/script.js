13470891c7fedea9ecd5b17f2ab4e98cf4db2dd3
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2024-01-02 16:28:35 +0530

    Oprators & Conditional , Statements
    Arithmetic, assignment, comparison, logical operators,
    Conditional Statements - if statement, if else statement

---
+++ lecture-2/1-Arithmatic_op/script.js
@@ -0,0 +1,34 @@
+// oprators in js -> binary & unary 
+// arithmatic opration + - * / , modulo(%), exponential(**),
+
+let a = 5;
+let b = 2;
+
+console.log("a = ", a , "b = ", b);
+console.log("a + b = ", a + b);
+console.log("a - b = ", a - b);
+console.log("a * b = ", a * b);
+console.log("a / b = ", a / b);
+console.log("a % b = ", a % b);
+console.log("a ** b = ", a ** b);
+
+// Unary Oprator 
+let c = 5;
+let d = 2;
+
+//post increment 
+console.log("c = ", c, "d = ", d);
+console.log("c++ = ", c++);
+console.log("c = ", c);
+
+// pre increament
+console.log("c = ", c, "d = ", d);
+console.log("++c = ", ++c);
+console.log("c = ", c);
+
+
+// decrement
+console.log("c = ", c, "d = ", d);
+console.log("c-- = ", c--);
+console.log("c = ", c);
+
