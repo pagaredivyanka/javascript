7c90ee9320cc85c2936edb495b3d2d74c2edee6c
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2024-08-17 00:06:36 +0530

    JS

---
+++ JS/L02-Numbers-And-Math/exercise.js
@@ -0,0 +1,58 @@
+/* Q. 2a -> At, restaurant, you order 
+1 soup => $10 
+3 burgers => $8
+1 ice cream => $5
+total cost =?
+
+and -> 
+
+(10 * 1) + (8 * 3) + (5 * 1) = 39 
+
+total cost = $39 
+
+*/
+
+/*
+Q. 2b 
+3 people 
+1 person orders ->
+1 soup => $10 
+3 burgers => $8
+1 ice cream => $5
+
+each person pays = $39 
+
+*/ 
+
+/*
+Q. 2c
+1 toaster = $18.50
+2 shirts = $7.50 (each)
+
+total = ?
+
+(1850 + (750 * 2)) / 100
+= $33.5
+
+*/
+
+/*
+Q. 2d
+10% tax for total price ($33.5)
+750 * 2 
+= 1500
+(1850 + 1500) * 0.1 / 100
+tax for total price = $3.35
+
+*/
+
+
+/*
+Q. 2e
+20% tax for total price ($33.5)
+750 * 2 
+= 1500
+(1850 + 1500) * 0.2 / 100
+tax for total price = $6.7
+
+*/
