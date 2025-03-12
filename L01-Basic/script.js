1bf6679ae427ffa4ae995db500e4498c8662a5b1
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2025-03-12 12:20:10 +0530

    Add files via upload

---
+++ L01-Basic/script.js
@@ -0,0 +1,22 @@
+// alert('hello');  //show in the pop up form
+
+// document.body.innerHTML = 'Hello';  //write in console it gives o/p in console and on web page 
+
+// exercies - (performed on console)
+
+// Popup
+alert('Good Morning');
+alert('Ritesh');
+alert('Divyanka');
+
+
+// Math in Console 
+
+// > 10 + 5
+// < 15
+
+// > 20 - 5
+// < 15
+
+// > 2 + 2 - 5
+// < -1
---
+++ L03-String/script.js
@@ -0,0 +1,47 @@
+/*
+Strings 
+
+
+'$' + 28.94 + 7.99
+'$28.947.99'
+
+'$' + (28.94 + 7.99)
+'$36.93'
+
+'$' + (20.94 + 7.99)
+'$28.93'
+
+'Item (' + (1 + 1) +'): $' + (20.94 + 7.99)
+'Item (2): $28.93'
+
+alert('Item (' + (1 + 1) +'): $' + (20.94 + 7.99));
+
+'Item'
+"Item"
+"I' am learning Jacascript"
+'I\' am learning Javascript'
+
+//Interpolation
+`Items (${1 + 1}): $${(2095 + 799) / 100}`
+'Items (2): $28.94'
+
+
+`some`
+'some'
+
+//multi-line string =
+`some
+text`
+'some\ntext'
+
+
+` hello
+all`
+
+output -> 'hello\nall'
+
+' ' -> used by default for strings 
+` ` -> for interpolation, multi-lie strigns 
+
+
+*/
